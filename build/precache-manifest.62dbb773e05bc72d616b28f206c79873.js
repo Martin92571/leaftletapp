d8f3acd86215dbac7af7d7b882533903ac774414
Martin Rizo <35642313+Martin92571@users.noreply.github.com>
2019-10-24 07:40:20 -0700

    Added Build File

---
+++ build/precache-manifest.62dbb773e05bc72d616b28f206c79873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc941c9463441573001cf0547a039121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4c637f5ca71828c36d2",
+    "url": "/static/css/main.3a86a8d1.chunk.css"
+  },
+  {
+    "revision": "b16d7548a9645b569e66",
+    "url": "/static/js/2.434b6bc8.chunk.js"
+  },
+  {
+    "revision": "c4c637f5ca71828c36d2",
+    "url": "/static/js/main.81dc7d5e.chunk.js"
+  },
+  {
+    "revision": "7973ce345e7f745e6235",
+    "url": "/static/js/runtime-main.2bf6e4d0.js"
+  }
+]);
